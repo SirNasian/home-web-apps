3494d5bb28a940c951dbf322b77848046e0fdde8
Nathan Iszlaub <NathanIszlaub@gmail.com>
2021-07-11 16:34:46 +0800

    Display notes overview instead of mock data

--- src/components/NotesOverview.tsx
+++ src/components/NotesOverview.tsx
@@ -5,58 +5,31 @@ import { useTheme } from '@material-ui/core/styles';
 
 import moment from 'moment';
 
-const mock_data = [
-	{
-		title: 'Shopping List',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'To Do',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'Timesheet',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'Wedding',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'Budget',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'Makki',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'House',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'Birthday',
-		date: new Date().toISOString(),
-	},
-	{
-		title: 'Thoughts',
-		date: new Date().toISOString(),
-	},
-];
+interface OverviewItem {
+	id: string;
+	title: string;
+	date: string;
+}
 
 export const NotesOverview = ({
 	searchText,
 }: {
 	searchText: string;
 }): JSX.Element => {
+	const [items, setItems] = React.useState<OverviewItem[]>([]);
+	React.useEffect(() => {
+		window.fetch('/api/notes/overview')
+			.then((res) => res.json())
+			.then((data) => setItems(data));
+	}, [])
 	return (
 		<Grid container spacing={1}>
-			{mock_data
-				.filter((item) =>
+			{items
+				.filter((item: OverviewItem) =>
 					item.title.toUpperCase().includes(searchText.toUpperCase())
 				)
-				.map((item, index) => (
-					<Grid item xs={12} key={index}>
+				.map((item) => (
+					<Grid item xs={12} key={item.id}>
 						<NotesOverviewCard title={item.title} date={item.date} />
 					</Grid>
 				))}
